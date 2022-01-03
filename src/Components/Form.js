40adf0bf67b0f08ba3513fdb5566029861ba11f5
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-03 11:55:10 +0530

    form added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+     const [values, setValues] = useState({
+         firstName: "",
+         lastName: "",
+         email: "",
+
+     })
+
     return (
         <div className="form-wrapper">
+            <form>
+            <input 
+            className="field-form"
+            placeholder="First Name"
+            value={values.firstName}
+            /> <br />
+              <input 
+            className="field-form"
+            placeholder="Last Name"
+            value={values.lastName}
+            /><br />
+              <input 
+            className="field-form"
+            placeholder="Email"
+            value={values.email}
+            /><br />
+            </form>
             
         </div>
     )

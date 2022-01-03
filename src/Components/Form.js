31f95ae2b2b88c725d9ceb107e3d88145b891cc7
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-03 13:03:38 +0530

    styling 2

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -18,28 +18,34 @@ function Form() {
             placeholder="First Name"
             value={values.firstName}
             /> <br />
+            <span className="span-field">Please enter First Name</span><br />
               <input 
             className="field-form"
             placeholder="Last Name"
             value={values.lastName}
             /><br />
+            <span className="span-field">Please enter Last Name</span><br />
               <input 
             className="field-form"
             placeholder="Email"
             value={values.email}
             /><br />
+            <span className="span-field">Please enter Email</span><br />
              <input 
             className="field-form"
             placeholder="Phone Number"
             value={values.phoneNumber}
             /><br />
+            <span className="span-field">Please enter Phone Number</span><br />
              <input 
             className="field-form"
             placeholder="City"
             value={values.city}
             /><br />
+            <span className="span-field">Please enter City</span><br />
             <button
             className="field-form"
+            style={{width: 100}}
             >Submit</button>
             </form>
             
